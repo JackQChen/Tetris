6908a33044951b4a98d750571b71469ce8c28266
Jack Chen <jackqchen@outlook.com>
2025-03-05 10:20:47 +0800

    Update Logger

--- App/TetrisPi/Logger.cs
+++ App/TetrisPi/Logger.cs
@@ -2,24 +2,20 @@
 {
     public class Logger
     {
-        private static readonly string logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt");
+        private static readonly string logFilePath;
         private static StreamWriter writer;
 
         static Logger()
         {
+            logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt");
+
             if (File.Exists(logFilePath))
                 File.Delete(logFilePath);
 
             writer = new StreamWriter(logFilePath, true) { AutoFlush = true };
         }
 
-        public static void Log(string message)
-        {
-            message = message.Trim('\0');
-            if (string.IsNullOrWhiteSpace(message))
-                return;
-            writer.WriteLine(message);
-        }
+        public static void Log(string message) => writer.WriteLine(message);
 
         public static void Close() => writer.Close();
     }
